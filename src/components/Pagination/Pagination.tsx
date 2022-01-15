f7216928ff68f1782d7255647e74cc7bb04db216
Carlos Henrique de Lima <carlos334lima@gmail.com>
2022-01-15 20:43:19 -0300

    Componente: Pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@chakra-ui/button";
+import { Box, Stack } from "@chakra-ui/react";
+
+interface PaginationItemProps {
+  number: number;
+  isCurrent?: boolean;
+  onPageChange: (page: number) => void;
+}
+
+export function PaginationItem({
+  isCurrent = true,
+  onPageChange,
+  number,
+}: PaginationItemProps) {
+  return (
+    <Stack
+      direction="row"
+      mt="8"
+      align="center"
+      justify="space-between"
+      spacing="6"
+    >
+      <Box>
+        <strong> 0 </strong> - <strong> 10 </strong> de <strong> 100 </strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{
+            bg: "pink.500",
+            cursor: "default",
+          }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bg="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          2
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bg="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          3
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import NextLink from "next/link";
+import { GetServerSideProps } from "next";
+
+//@libraries
+import { RiAddLine } from "react-icons/ri";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Link,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+//@components
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { PaginationItem } from "../../components/Pagination/Pagination";
+
+export default function UserList({ users }) {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <NextLink href="/users/create">
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </NextLink>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Link color="purple.400">
+                      <Text fontWeight="bold">Carlos Lima</Text>
+                    </Link>
+                    <Text fontSize="sm" color="gray.300">
+                      carlos334lima@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de maio, 2022</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <PaginationItem
+                totalCountOfRegisters={10}
+                currentPage={2}
+              />
+        </Box>
+      </Flex>
+    </Box>
+  );
+}

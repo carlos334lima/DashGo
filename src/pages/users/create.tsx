ef96bde5d567bb9f6f16a4cd59f2edc9be18e4ec
Carlos Henrique de Lima <carlos334lima@gmail.com>
2022-01-16 10:26:51 -0300

    create user

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+//@libraries
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+
+//@components
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { InputBase } from "../../components/Form/Input";
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box as="form" flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <InputBase name="name" label="Nome completo" />
+              <InputBase name="email" type="email" label="E-mail" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <InputBase name="password" type="password" label="Senha" />
+              <InputBase
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
+              <Button type="submit" colorScheme="pink">
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
